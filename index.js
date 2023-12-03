51217a6dab46e8d17d6f47e1edc57c7b2bad6733
André de Paula Terceiro <andre@terceiro.com.br>
2023-12-03 14:52:12 -0300

    All - v1.0.0.0

---
+++ index.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+const csvParser = require("csv-parser");
+
+const result = [];
+
+fs.createReadStream("./base.csv")
+  .pipe(csvParser())
+  .on("data", (data) => {
+    result.push(data);
+  })
+  .on("end", () => {
+    //console.log(result);
+
+    //console.log(result[0]['Telefone(s)'])
+
+    console.log(adjustCellphone(result[0]['Telefone(s)']));
+  });
+
+  function adjustCellphone(cellphone) {
+    return "+55" + cellphone.substr(10,14)
+        .replace(')', '')
+        .replace(' ', '')
+        .replace('-', ''); 
+  }
